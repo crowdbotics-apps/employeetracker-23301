e0bc3ede6f40ef5447f2433719709d168ce47107
Crowdbotics <team@crowdbotics.com>
2020-12-10 15:58:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/employeetracker_23301/MainActivity.java
@@ -0,0 +1,15 @@
+package com.employeetracker_23301;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "employeetracker_23301";
+    }
+}
